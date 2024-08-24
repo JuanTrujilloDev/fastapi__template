383c0947b043e540d28b13711ad38c4b0607b4b8
Juan Trujillo <juantrujillo.dev@gmail.com>
2024-08-23 23:38:35 -0500

    feat: catch possible errors

--- manage.py
+++ manage.py
@@ -13,9 +13,17 @@ import sys
 def main():
     """Run manage commands located in scripts directory."""
 
+    if not len(sys.argv) > 1:
+        raise ValueError("Please provide a command to run.")
+
     command = sys.argv[1]
-    module = importlib.import_module(f"fastapi__template.scripts.{command}")
-    module.main()
+    try:
+        module = importlib.import_module(f"fastapi__template.scripts.{command}")
+        module.main()
+    except ModuleNotFoundError:
+        raise ValueError(f"Command {command} not found.")
+    except AttributeError:
+        raise ValueError(f"Command {command} does not have a main function.")
 
 
 if __name__ == "__main__":
