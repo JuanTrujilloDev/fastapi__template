8bb0fc1d110cfaa6785a85ef21ee8abc2c4221bf
Juan Trujillo <juantrujillo.dev@gmail.com>
2024-08-24 00:56:25 -0500

    style: reformat code

--- fastapi__template/test_setup.py
+++ fastapi__template/test_setup.py
@@ -12,20 +12,28 @@ from sqlmodel import SQLModel
 from fastapi__template.app import app
 from fastapi__template.settings import SETTINGS
 
+test_db_name = SETTINGS.database_name + "_test"
 engine_url = (
     f"postgresql://{SETTINGS.database_user}:{SETTINGS.database_password}"
     f"@{SETTINGS.database_host}:{SETTINGS.database_port}"
 )
-engine = sa.create_engine(engine_url)
-test_db_name = SETTINGS.database_name + "_test"
-with engine.connect() as connection:
-    connection.execution_options(isolation_level="AUTOCOMMIT")
-    exists = connection.execute(
-        sa.text(f"SELECT 1 FROM pg_database WHERE datname = '{test_db_name}'")
-    ).fetchone()
-    if not exists:
-        connection.execute(sa.text(f"CREATE DATABASE {test_db_name}"))
 
+
+def prepare_test_database():
+    """Prepare test database."""
+
+    db_engine = sa.create_engine(engine_url)
+    with db_engine.connect() as connection:
+        connection.execution_options(isolation_level="AUTOCOMMIT")
+        exists = connection.execute(
+            sa.text(f"SELECT 1 FROM pg_database WHERE datname = '{test_db_name}'")
+        ).fetchone()
+        if not exists:
+            connection.execute(sa.text(f"CREATE DATABASE {test_db_name}"))
+    return db_engine
+
+
+engine = prepare_test_database()
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
