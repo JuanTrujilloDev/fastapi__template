6d43665941611d702773270b3bd7d26dc8729546
Juan Trujillo <juantrujillo.dev@gmail.com>
2024-08-24 01:28:31 -0500

    feat: lint script [TODO needs fix for pylint]

---
+++ scripts/lint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# Run black
+echo "Running black..."
+black . --check --config=./pyproject.toml
+
+# Run isort
+echo "Running isort..."
+isort . --check-only --profile=black
+
+# Running ruff
+echo "Running ruff..."
+ruff check --config=./pyproject.toml .
+
+# Running bandit
+echo "Running bandit..."
+bandit -c pyproject.toml -r .
+
+# Run linter
+echo "Running linter..."
+pylint . --rcfile=./pyproject.toml
