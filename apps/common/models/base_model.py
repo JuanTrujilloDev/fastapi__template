5fa31c8e74b4a8e281bde61e0a92a8d201c0a98e
Juan Trujillo <juantrujillo.dev@gmail.com>
2024-09-07 21:56:17 -0500

    feat: change implementation to tortoise

--- apps/common/models/base_model.py
+++ apps/common/models/base_model.py
@@ -6,38 +6,54 @@ This file is subject to the terms and conditions defined in file 'LICENSE',
 which is part of this source code package.
 """
 
+import asyncio
 import uuid
-from abc import ABC
-from datetime import datetime, timezone
-from functools import partial
-from typing import Any, Optional
-from uuid import UUID
+from datetime import datetime
+from typing import Awaitable, Callable, List, Optional
 
-from pydantic import ConfigDict
-from sqlmodel import Field, SQLModel
+from tortoise import fields, models
+
+CleanMethod = Callable[[], Awaitable[None]]
 
 
-class BaseModel(SQLModel, ABC):
+class BaseModel(models.Model):
     """Base model for all models"""
 
-    id: Optional[UUID] = Field(
-        default_factory=uuid.uuid4, primary_key=True, allow_mutation=False
+    id: Optional[uuid.UUID] = fields.UUIDField(
+        primary_key=True, nullable=False, default=uuid.uuid4, unique=True
     )
-    is_active: Optional[bool] = Field(default=True)
-    created_at: datetime = Field(default=datetime.now(timezone.utc), nullable=False)
-    updated_at: datetime = Field(
-        default_factory=partial(datetime.now, tz=timezone.utc), nullable=False
-    )
-
-    model_config = ConfigDict(
-        from_attributes=True, validate_default=True, validate_assignment=True
+    is_active: Optional[bool] = fields.BooleanField(default=True, nullable=False)
+    created_at: datetime = fields.DatetimeField(auto_now_add=True, nullable=False)
+    updated_at: datetime = fields.DatetimeField(
+        auto_now=True, auto_now_add=True, nullable=False
     )
 
-    def _set_skip_validation(self, name: str, value: Any) -> None:
-        """Workaround to be able to set fields without validation."""
-        attr = getattr(self.__class__, name, None)
-        if isinstance(attr, property):
-            attr.__set__(self, value)
-        else:
-            self.__dict__[name] = value
-            self.__pydantic_fields_set__.add(name)
+    class Meta:
+        """Meta class for BaseModel"""
+
+        abstract = True
+        ordering = ["-updated_at"]
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the base model"""
+        super().__init__(*args, **kwargs)
+        self.clean_methods: List[CleanMethod] = [
+            getattr(self, method)
+            for method in dir(self)
+            if method.startswith("clean_")
+            and isinstance(getattr(self, method), CleanMethod)
+        ]
+
+    def __str__(self):
+        """String representation of the model"""
+        return str(self.id)
+
+    async def clean(self):
+        """Clean the model"""
+        await asyncio.gather(*(method() for method in self.clean_methods))
+        return self
+
+    async def save(self, *args, **kwargs):
+        """Save the model"""
+        await self.clean()
+        super().save(*args, **kwargs)
