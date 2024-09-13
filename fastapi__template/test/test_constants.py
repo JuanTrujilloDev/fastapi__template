4696ff8645865914530692fe7d08be80629ef8a2
Juan Trujillo <juantrujillo.dev@gmail.com>
2024-09-12 21:32:17 -0500

    feat: move db commands to test constants

---
+++ fastapi__template/test/test_constants.py
@@ -0,0 +1,62 @@
+"""
+
+Test constants, such as test database name and test database engine,
+are defined in this file.
+
+This file is subject to the terms and conditions defined in file 'LICENSE',
+which is part of this source code package.
+"""
+
+from enum import StrEnum
+
+import sqlalchemy as sa
+
+from fastapi__template.database import get_engine
+from fastapi__template.settings import SETTINGS
+
+TEST_DB_NAME = SETTINGS.DATABASE_NAME + "_test"
+
+
+class DBExistsCommand(StrEnum):
+    """
+
+    Enum for database exists command.
+
+    Attributes:
+        postgresql: PostgreSQL database exists command.
+        mysql: MySQL database exists command.
+        mssql: Microsoft SQL Server database exists command.
+        postgresql_psycopg: PostgreSQL database exists command for psycopg.
+        postgresql_psycopg2: PostgreSQL database exists command for psycopg
+    """
+
+    postgresql = f"SELECT 1 FROM pg_database WHERE datname = '{TEST_DB_NAME}'"
+    mysql = (
+        "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA "
+        f"WHERE SCHEMA_NAME = '{TEST_DB_NAME}'"
+    )
+    mssql = f"SELECT 1 FROM sys.databases WHERE name = '{TEST_DB_NAME}'"
+    postgresql_psycopg = f"SELECT 1 FROM pg_database WHERE datname = '{TEST_DB_NAME}'"
+    postgresql_psycopg2 = f"SELECT 1 FROM pg_database WHERE datname = '{TEST_DB_NAME}'"
+
+    @classmethod
+    def get_command(cls, database_name: str) -> str:
+        """
+        Get database exists command.
+        """
+        try:
+            return cls[database_name].value
+        except (KeyError, AttributeError) as error:
+            raise ValueError(f"Invalid database engine {database_name}") from error
+
+
+TEST_DB_ENGINE = get_engine(
+    database_engine=SETTINGS.DATABASE_ENGINE_NAME,
+    database_name=SETTINGS.DATABASE_NAME,
+    database_user=SETTINGS.DATABASE_USER,
+    database_password=SETTINGS.DATABASE_PASSWORD,
+    database_host=SETTINGS.DATABASE_HOST,
+    database_port=SETTINGS.DATABASE_PORT,
+)
+
+engine = sa.create_engine(TEST_DB_ENGINE, poolclass=sa.StaticPool)
--- fastapi__template/test/test_setup.py
+++ fastapi__template/test/test_setup.py
@@ -10,38 +10,7 @@ import os
 
 import sqlalchemy as sa
 
-from fastapi__template.database import get_engine
-from fastapi__template.settings import SETTINGS
-
-TEST_DB_NAME = SETTINGS.DATABASE_NAME + "_test"
-EXISTS_COMMAND = {
-    "postgresql": (
-        f"SELECT 1 FROM pg_database WHERE datname = '{TEST_DB_NAME}'"  # nosec: B608
-    ),
-    "mysql": (
-        "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA "  # nosec: B608
-        f"WHERE SCHEMA_NAME = '{TEST_DB_NAME}'"  # nosec: B608
-    ),
-    "mssql": (
-        f"SELECT 1 FROM sys.databases WHERE name = '{TEST_DB_NAME}'"  # nosec: B608
-    ),
-    "postgresql+psycopg": (
-        f"SELECT 1 FROM pg_database WHERE datname = '{TEST_DB_NAME}'"  # nosec: B608
-    ),
-    "postgresql+psycopg2": (
-        f"SELECT 1 FROM pg_database WHERE datname = '{TEST_DB_NAME}'"  # nosec: B608
-    ),
-}
-TEST_DB_ENGINE = get_engine(
-    database_engine=SETTINGS.DATABASE_ENGINE_NAME,
-    database_name=SETTINGS.DATABASE_NAME,
-    database_user=SETTINGS.DATABASE_USER,
-    database_password=SETTINGS.DATABASE_PASSWORD,
-    database_host=SETTINGS.DATABASE_HOST,
-    database_port=SETTINGS.DATABASE_PORT,
-)
-
-engine = sa.create_engine(TEST_DB_ENGINE, poolclass=sa.StaticPool)
+from fastapi__template.test.test_constants import TEST_DB_NAME, DBExistsCommand, engine
 
 
 def prepare_test_database():
@@ -50,9 +19,9 @@ def prepare_test_database():
     connection.execution_options(isolation_level="AUTOCOMMIT")
     exists = connection.execute(
         sa.text(
-            EXISTS_COMMAND[os.environ.get("DATABASE_ENGINE", "postgresql")].format(
-                database_name=TEST_DB_NAME
-            )
+            DBExistsCommand.get_command(
+                os.environ.get("DATABASE_ENGINE", "postgresql")
+            ).format(database_name=TEST_DB_NAME)
         )
     ).fetchone()
     if not exists:
@@ -68,9 +37,9 @@ def drop_test_database():
     connection.execution_options(isolation_level="AUTOCOMMIT")
     exists = connection.execute(
         sa.text(
-            EXISTS_COMMAND[os.environ.get("DATABASE_ENGINE", "postgresql")].format(
-                database_name=TEST_DB_NAME
-            )
+            DBExistsCommand.get_command(
+                os.environ.get("DATABASE_ENGINE", "postgresql")
+            ).format(database_name=TEST_DB_NAME)
         )
     ).fetchone()
     if exists:
