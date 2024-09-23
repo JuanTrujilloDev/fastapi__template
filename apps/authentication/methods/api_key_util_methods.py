adaff49ebb564f0ee05cd257403bd709b4a32cbb
Juan Trujillo <juantrujillo.dev@gmail.com>
2024-09-22 22:19:12 -0500

    feat: store api key with salt

--- apps/authentication/methods/api_key_util_methods.py
+++ apps/authentication/methods/api_key_util_methods.py
@@ -6,16 +6,28 @@ This file is subject to the terms and conditions defined in file 'LICENSE',
 which is part of this source code package.
 """
 
-import base64
 import hashlib
-import hmac
+import os
+import secrets
 
-from fastapi__template.settings import SETTINGS
+
+def generate_api_key() -> str:
+    """Generate an API key."""
+    return secrets.token_urlsafe(32)
 
 
 def hash_api_key(api_key: str) -> str:
-    """Hash API key."""
-    base64_encoded_key = base64.b64encode(bytes(api_key, "utf-8"))
-    base64_encoded_secret_key = base64.b64encode(bytes(SETTINGS.SECRET_KEY, "utf-8"))
-    hashed_key = hmac.new(base64_encoded_secret_key, base64_encoded_key, hashlib.sha256)
-    return hashed_key.hexdigest()
+    """Hash an API key."""
+    salt = os.urandom(16)
+    hashed_key = hashlib.pbkdf2_hmac("sha256", api_key.encode("utf-8"), salt, 100000)
+    return f"{salt.hex()}:{hashed_key.hex()}"
+
+
+def verify_api_key(stored_hashed_key: str, provided_api_key: str) -> bool:
+    """Verify an API key."""
+    salt_hex, stored_hash_hex = stored_hashed_key.split(":")
+    salt = bytes.fromhex(salt_hex)
+    hashed_provided_key = hashlib.pbkdf2_hmac(
+        "sha256", provided_api_key.encode("utf-8"), salt, 100000
+    )
+    return stored_hash_hex == hashed_provided_key.hex()
